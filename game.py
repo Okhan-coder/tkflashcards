9d5c4b15664f0293eedfa8d1f18224b13804cbeb
Ilya Kavalerov <ilya@artsymail.com>
2016-07-27 14:00:11 -0400

    Game working with answers, now just need to check parsing

--- game.py
+++ game.py
@@ -1,20 +1,37 @@
+import random
+
 from Tkinter import *
 import Tkinter as tk
+
+import sympy
+from PIL import Image, ImageTk
 
 from parse_notes import Questions
 
 import pdb
 
+h1 = ('Helvetica', 24)
+h2 = ('Helvetica', 18)
+p = ('Times', 16)
+
 class Quiz(Frame):
     def __init__(self, master=None, sourcefile=None):
         Frame.__init__(self, master)
         self.pack()
-
+        # window settings
+        master.resizable(width=False, height=False)
+        master.geometry('{}x{}'.format(700, 400))
+        # get content
+        qs = Questions(sourcefile).questions
+        random.shuffle(qs)
+        # quiz states
+        self.idx = 0
         self.last_question = None # i.e. post next question if None
-        self.qq = iter(Questions(sourcefile).questions) # question queue
+        self.num_questions = len(qs)
+        self.qq = enumerate(qs) # question queue
 
         # graphical components
-        self.title = tk.Label(master,text = "Quiz")
+        self.title = tk.Label(master,text = 'Quiz (%i questions)' % self.num_questions, font=h1)
         self.title.pack()
 
         self.status = tk.Label(master,text = '')
@@ -33,19 +50,29 @@ class Quiz(Frame):
         self.next_frame()
 
     def next_frame(self):
-        if self.last_question: # show answer
-            self.status.config(text = 'Answer:')
+        if self.last_question:
+            # show answer with latex
+            ltx = self.last_question['A']
+            sympy.preview(ltx, viewer='file', filename='qtemp.jpg', euler=False)
+            image = Image.open('qtemp.jpg')
+            photo = ImageTk.PhotoImage(image)
+            self.answer.image = photo
+            self.answer.config(image = photo)
+            self.answer.pack()
+            # state maintenance
+            self.status.config(text = 'Answer (%i)' % (self.idx+1),
+                font=h2)
             self.fbutton.config(text = 'Next Question')
-            self.answer.config(text = self.last_question['A'])
-
             self.last_question = None
-        else: # post new question
-            self.status.config(text = 'Question:')
+        else:
+            # post new question
+            self.idx, newq = self.qq.next()
+            self.question.config(text = newq['Q'], font=p)
+            # state maintenance
+            self.status.config(text = 'Question (%i)' % (self.idx+1),
+                font=h2)
             self.fbutton.config(text = 'See Answer')
-            newq = self.qq.next()
-            self.question.config(text = newq['Q'])
-            self.answer.config(text = '')
-
+            self.answer.image = None
             self.last_question = newq
 
 if __name__ == '__main__':
