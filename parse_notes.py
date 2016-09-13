3eb523013cc65844d5a46453b4673464f2f61e6c
Ilya Kavalerov <ilya@artsymail.com>
2016-09-13 16:02:45 -0400

    Add feature to export flashcards as pdf

--- parse_notes.py
+++ parse_notes.py
@@ -1,4 +1,9 @@
 import re
+import random
+import os
+import sys
+
+import sympy
 
 import pdb
 
@@ -35,6 +40,75 @@ class Questions:
                         questions.append(question)
         return questions
 
+    # Caption - Word Wrapped Label, from: http://www.imagemagick.org/Usage/text/#label
+    def export_flashcard_text(self, text, filename):
+        cmd = 'convert -background White -pointsize 35 -size 667x375 -gravity Center caption:"%s" %s'  % (text, filename)
+        os.system(cmd)
+        return 1
+
+    def export_flashcard_image(self, imgfile, filename):
+        # just pad it
+        cmd = 'convert %s -gravity center -background white -extent 667x375 %s' % (imgfile, filename)
+        os.system(cmd)
+        return 1
+
+    # overwrites!
+    # use this on every answer so it is easy to tell which is the question and which is the answer
+    # http://www.imagemagick.org/Usage/annotating/
+    def add_text_to_image(self, text, imgfile):
+        cmd2 = 'convert -background "#0008" -fill white -gravity center -pointsize 25 -size 667x caption:" %s " %s +swap -gravity north -composite  %s' % (text, imgfile, imgfile)
+        os.system(cmd2)
+        return 1
+
+    # for cmd2: http://www.imagemagick.org/Usage/annotating/
+    # def export_flashcard_text_w_image(self, text, imgfile, filename):
+    #     # just pad it
+    #     cmd = 'convert %s -gravity center -background white -extent 667x375 %s' % (imgfile, filename)
+    #     os.system(cmd)
+    #     # add text
+    #     # cmd = 'convert %s  -fill white  -undercolor "#00000080"  -gravity South -annotate +0+5 " %s " %s' % (filename, text, filename)
+    #     cmd2 = 'convert -background "#0008" -fill white -gravity center -pointsize 25 -size 667x caption:"%s" %s +swap -gravity north -composite  %s' % (text, filename, filename)
+    #     os.system(cmd2)
+    #     return 1
+
+    def export_flashcards(self, outpdf):
+        qs = self.questions
+        # random.shuffle(qs)
+
+        os.system('rm flash_export_tmp/*.png')
+
+        dr = 'flash_export_tmp/'
+        i = 0;
+        tmpfile = ('%sqtemp.png' % dr)
+        for q in qs:
+            if q['type'] == 'text':
+                # question
+                i += self.export_flashcard_text(q['Q'], "%s%03i.png" % (dr, i))
+
+                self.export_flashcard_text(q['A'], "%s%03i.png" % (dr, i))
+                i += self.add_text_to_image(q['Q'], "%s%03i.png" % (dr, i))
+            elif q['type'] == 'latex':
+                try:
+                    sympy.preview(q['A'], viewer='file', filename=tmpfile, euler=False)
+                    i += self.export_flashcard_text(q['Q'], "%s%03i.png" % (dr, i))
+
+                    self.export_flashcard_image(tmpfile, "%s%03i.png" % (dr, i))
+                    i += self.add_text_to_image(q['Q'], "%s%03i.png" % (dr, i))
+                except:
+                    print 'bad question answer:\n%s\n%s' % (q['Q'], q['A'])
+        os.system('rm %s' % tmpfile)
+        # combine into pdf
+        # pdf_cmd = "ls %s/*.png | sort -n | tr '\n' ' ' | sed 's/$/\ %s/' | xargs convert" % (dr, outpdf)
+        pdf_cmd = 'convert %s/*.png %s' % (dr, outpdf)
+        os.system(pdf_cmd)
+
+
 if __name__ == '__main__':
-    qq = Questions('../test.md')
-    pdb.set_trace()
+    if len(sys.argv) == 1:
+        qq = Questions('../test.md')
+    elif len(sys.argv) == 3:
+        infile = sys.argv[1]
+        outfile = sys.argv[2]
+        qq = Questions(infile)
+        qq.export_flashcards(outfile)
+    # pdb.set_trace()
